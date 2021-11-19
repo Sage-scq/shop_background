44377abe11822392bb63e5192d31d8c0291e8b9d
Sage-scq <1637718646@qq.com>
2021-11-19 21:16:04 +0800

    更新商品管理

--- src/views/product/trademark/List.vue
+++ src/views/product/trademark/List.vue
@@ -36,6 +36,36 @@
       @current-change="getTrademarkList"
     >
     </el-pagination>
+
+    <el-dialog title="添加品牌" :visible.sync="dialogFormVisible">
+      <el-form :model="tmForm" style="width: 80%">
+        <el-form-item label="品牌名称" label-width="100px">
+          <el-input v-model="tmForm.tmName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="100px">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div class="el-upload__tip" slot="tip">
+              只能上传jpg/png文件，且不超过500kb
+            </div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -48,6 +78,13 @@ export default {
       limit: 3,
       trademarkList: [],
       total: 0,
+      // 控制dialog是否显示
+      dialogFormVisible: true,
+      // 增加功能收集的数据
+      tmForm: {
+        tmName: "",
+      },
+      imageUrl: "",
     };
   },
   mounted() {
@@ -69,9 +106,48 @@ export default {
         this.total = result.data.total;
       }
     },
+    // 上传相关的回调
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped >
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
