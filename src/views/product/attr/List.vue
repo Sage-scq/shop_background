fb4b543979501926c20f02ad7e3c71ef974e0d93
Sage-scq <1637718646@qq.com>
2021-11-23 16:28:37 +0800

    属性管理

--- src/views/product/attr/List.vue
+++ src/views/product/attr/List.vue
@@ -5,11 +5,124 @@
       <CategorySelector @changeCategory="changeCategory" />
     </el-card>
     <!-- 列表 -->
-    <el-card style="margin-top: 20px"> </el-card>
+    <el-card style="margin-top: 20px">
+      <div v-show="isShowList">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          style="margin-bottom: 10px"
+          @click="showAddDiv"
+          :disabled="!category3Id"
+          >添加属性</el-button
+        >
+        <el-table :data="attrList" style="width: 100%" border>
+          <el-table-column label="序号" width="80" type="index" align="center">
+          </el-table-column>
+          <el-table-column prop="attrName" label="属性名称" width="150">
+          </el-table-column>
+          <el-table-column label="属性值列表" width="width">
+            <template slot-scope="{ row }">
+              <el-tag
+                style="margin-right: 10px"
+                type="info"
+                v-for="attrValue in row.attrValueList"
+                :key="attrValue.id"
+                >{{ attrValue.valueName }}</el-tag
+              >
+            </template>
+          </el-table-column>
+          <el-table-column prop="prop" label="操作" width="150">
+            <template slot-scope="{ row }">
+              <el-tooltip content="修改" placement="top">
+                <el-button
+                  icon="el-icon-edit"
+                  type="warning"
+                  size="small"
+                  @click="showUpdateDiv(row)"
+                ></el-button>
+              </el-tooltip>
+              <el-tooltip content="删除" placement="top">
+                <el-button
+                  icon="el-icon-delete"
+                  type="danger"
+                  size="small"
+                ></el-button>
+              </el-tooltip>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <!-- 添加/修改页面 -->
+      <div v-show="!isShowList">
+        <el-form ref="form" :model="attrForm" :inline="true">
+          <el-form-item label="属性名">
+            <el-input
+              v-model="attrForm.attrName"
+              placeholder="请输入属性名"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="!attrForm.attrName"
+          @click="addAttrValue"
+          >添加属性值</el-button
+        >
+        <el-button @click="isShowList = true">取消</el-button>
+        <el-table
+          style="width: 100%; margin: 20px 0"
+          border
+          :data="attrForm.attrValueList"
+        >
+          <el-table-column type="index" align="center" label="序号" width="80">
+          </el-table-column>
+          <el-table-column prop="prop" label="属性值名称" width="width">
+            <template slot-scope="{ row, $index }">
+              <el-input
+                :ref="$index"
+                v-if="row.isEdit"
+                v-model="row.valueName"
+                placeholder="请输入属性值名称"
+                @blur="toLook(row)"
+                @keyup.enter.native="toLook(row)"
+                size="small"
+              ></el-input>
+              <span
+                v-else
+                @click="toEdit(row, $index)"
+                style="display: block; width: 100%; height: 100%"
+                >{{ row.valueName }}</span
+              >
+            </template>
+          </el-table-column>
+          <el-table-column prop="prop" label="操作" width="width">
+            <template slot-scope="{ row, $index }">
+              <el-tooltip effect="dark" content="删除" placement="top">
+                <el-popconfirm
+                  :title="`确定删除${row.valueName}吗？`"
+                  @onConfirm="attrForm.attrValueList.splice($index, 1)"
+                >
+                  <el-button
+                    slot="reference"
+                    icon="el-icon-delete"
+                    size="mini"
+                    type="danger"
+                  ></el-button>
+                </el-popconfirm>
+              </el-tooltip>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShowList = true">取消</el-button>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
+import cloneDeep from "lodash/cloneDeep";
 export default {
   name: "",
   data() {
@@ -18,9 +131,78 @@ export default {
       category2Id: "",
       category3Id: "",
       attrList: [],
+      isShowList: true,
+      attrForm: {
+        attrName: "",
+        attrValueList: [],
+        categoryId: 0,
+        categoryLevel: 3,
+      },
     };
   },
   methods: {
+    // 切换为编辑模式
+    toEdit(row, index) {
+      row.isEdit = true;
+      // 不能获取的太快，需要等创建完成再进行获取
+      this.$nextTick(() => {
+        this.$refs[index].focus();
+      });
+    },
+    // 切换为察看模式
+    toLook(row) {
+      // 数据不能为空
+      if (row.valueName.trim() === "") {
+        this.$message.info("输入不能为空");
+        row.valueName = "";
+        return;
+      }
+      // 判断不重复
+      let isRepeat = this.attrForm.attrValueList.some((item) => {
+        if (item !== row) {
+          return item.valueName === row.valueName;
+        }
+      });
+      if (!isRepeat) {
+        row.isEdit = false;
+      } else {
+        this.$message.info("输入不能重复");
+        row.valueName = "";
+        return;
+      }
+    },
+    // 点击切换到修改页面
+    showUpdateDiv(row) {
+      this.isShowList = false;
+      // 深拷贝，让修改的和显示的不是同一个数据
+      this.attrForm = cloneDeep(row);
+      this.attrForm.attrValueList.forEach((attr) => {
+        this.$set(attr, "isEdit", false);
+      });
+    },
+    // 点击切换到添加页面
+    showAddDiv() {
+      this.isShowList = false;
+      this.attrForm = {
+        attrName: "",
+        attrValueList: [],
+        categoryId: this.category3Id,
+        categoryLevel: 3,
+      };
+    },
+    // 点击添加属性值
+    addAttrValue() {
+      // 首先添加一个空对象
+      this.attrForm.attrValueList.push({
+        attrId: this.attrForm.id,
+        valueName: "",
+        isEdit: true,
+      });
+      // 获取焦点
+      this.$nextTick(() => {
+        this.$refs[this.attrForm.attrValueList.length - 1].focus();
+      });
+    },
     changeCategory({ categoryId, level }) {
       // 保存子组件传递的ID
       if (level === 1) {
